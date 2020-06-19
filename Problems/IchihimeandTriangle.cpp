f571e8fa711d47ded623f4a81194d7ef7fed9c82
R3CEPT0R <lestr3959@gmail.com>
2020-06-18 21:42:35 -0500

    Create IchihimeandTriangle.cpp

---
+++ Problems/IchihimeandTriangle.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int a, b, c, d, t;
+	cin >> t;
+	while (t-- != 0)
+	{
+		cin >> a >> b >> c >> d;
+		cout << b <<" "<< c <<" "<< c << endl;
+	}
+}
